e07dfebecc09b891e44d7cb0bdf4a6f18153084c
Snowball <jan.schneider1988@gmail.com>
2019-01-31 10:47:03 +0100

    initial commit

---
+++ mod.lua
@@ -0,0 +1,24 @@
+function data()
+
+	return {
+		info = {
+			minorVersion = 0,
+			severityAdd = "NONE",
+			severityRemove = "NONE",
+			name = _("title"),
+			description = _("description"),
+			tags = {"Misc", "Script Mod", "Shader"},
+			authors = {
+				{
+					name = 'Snowball',
+					role = 'CREATOR',
+					text = 'Script',
+				},
+			},
+			
+			visible = true,
+		},
+		
+	}
+
+end
---
+++ strings.lua
@@ -0,0 +1,13 @@
+function data()
+	return {
+	de = {		
+		title = "Gletscher",
+		description = "Erstellt in Höhenlagen Schneefelder",
+		
+	},
+	en = {
+		title = "Glaciers",
+		description = "Creates snow fields in mountain areas",
+	},
+}
+end
